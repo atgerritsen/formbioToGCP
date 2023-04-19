b4782a4281ca17b6f82e8b756214e27cf97b3e66
atgerritsen <8368007+atgerritsen@users.noreply.github.com>
2023-04-19 10:28:49 -0600

    Add files via upload

--- dataTransferFormToGCP.sh
+++ dataTransferFormToGCP.sh
@@ -8,4 +8,19 @@ sudo ln -s $HOME/.local/bin/formbio /usr/local/bin/formbio
 
 formbio storage cp -r formbio://colossal/avian/shapiro_lab/seq/PE .
 
-# this then errors out after downloading 1 bacth. 
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash miniconda.sh
+
+screen -S #name
+
+# cleaned up and renamed a lot of files, then uploaded the fgf10 sequence from chicken
+# conda install -n bioconda bwa
+
+bwa index chicken_fgf10DNA.fa
+
+ls | sort | grep "_R1" > sortedR1.txt
+
+# april 14 2023 - copying over and mapping trimmed and filtered Dodo reads to see if there is a quality difference
+
+formbio storage cp -r formbio://colossal/avian/DodoAnalysis/filtered_reads/Dodo_PE_processed.fq .
+
